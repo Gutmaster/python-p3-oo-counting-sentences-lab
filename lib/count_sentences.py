8a5153fc99d95e165a79e2d4192cf0b220fa1cba
Chevy <vallchevy@gmail.com>
2024-06-10 14:52:35 -0300

    complete lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python3
+TERMINATING_CHARS = ['.', '?', '!']
 
 class MyString:
-  pass
+  def __init__(self, value = ""):
+    if isinstance(value, str):
+      self.value = value
+    else:
+      print('The value must be a string.')
+      self.value = ""
+  
+  def is_sentence(self):
+    return self.value[-1] == "."
+  
+  def is_question(self):
+    return self.value[-1] == "?"
+  
+  def is_exclamation(self):
+    return self.value[-1] == "!"
+  
+  def count_sentences(self):
+    count = 0
+    lastChar = ''
+    for char in self.value:
+      if char in TERMINATING_CHARS and not lastChar in TERMINATING_CHARS:
+        count += 1
+      lastChar = char
+    return count
+  
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value(self, value):
+    if isinstance(value, str):
+      self._value = value
+    else:
+      print('The value must be a string.')
